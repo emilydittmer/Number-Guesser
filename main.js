f2e5006262d2b7f287e9b32048a47d195928a73d
Hannah Hyde <hannahhyde@gmail.com>
2019-01-31 13:05:27 -0700

    Add min max range input function

--- main.js
+++ main.js
@@ -8,5 +8,20 @@ var updateBtn = document.querySelector('#update-btn');
 var guessBtn = document.querySelector('#guess-btn');
 var clearBtn = document.querySelector('#clear-btn');
 var resetBtn = document.querySelector('#reset-btn');
-var min = 1;
-var max = 100;
+var randomNum
+
+updateBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  generateNumber();
+});
+
+function generateNumber() {
+  var min = parseInt(minRange.value) || 1;
+  var max = parseInt(maxRange.value) || 100;
+  randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(randomNum);
+  var updateMin = document.querySelector('.update-min');
+  var updateMax = document.querySelector('.update-max');
+  updateMin.innerText = min;
+  updateMax.innerText = max;
+};
